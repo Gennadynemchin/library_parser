f81906a4de3040197741d2384e579380433eb481
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-05-03 18:20:03 +0300

    Added cover parsing;

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin
 
 
 def check_for_redirect(response):
@@ -15,7 +16,7 @@ def check_for_redirect(response):
 folder = 'books'
 Path(folder).mkdir(exist_ok=True)
 
-for book in range(1, 11):
+for book in range(13, 14):
     download_url = f'https://tululu.org/txt.php?id={book}'
     title_author_url = f'https://tululu.org/b{book}'
     try:
@@ -27,6 +28,10 @@ for book in range(1, 11):
 
         soup = BeautifulSoup(title_author_response.text, 'lxml')
         title_author = soup.find('h1').text.split('::')
+        cover = soup.find(class_='bookimage').find('img')['src']
+        print(cover)
+        cover_url = urljoin('https://tululu.org', cover)
+        print(cover_url)
         title = title_author[0].strip()
         author = title_author[1].strip()
         output_filename = sanitize_filename(title)
