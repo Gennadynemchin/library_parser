6d6d3dcbb56fa91f74930c694b3df340c94f3ee2
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-05-04 16:29:22 +0300

    Added README.md;

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# library_parser
+# Book parser tutulu.org
+
+### How to install
+
+```
+git clone https://github.com/Gennadynemchin/library_parser.git
+```
+
+Install requested libraries:
+```
+pip install requirements.txt
+```
+
+### How to start
+
+You have to use positional arguments while you are running the script. Example:
+```
+python main.py 20 30
+```
+After running the script downloading books from id=20 to id=30 starts.
--- main.py
+++ main.py
@@ -1,23 +1,11 @@
+import argparse
+import os
+import requests
 from requests import HTTPError
 from pathlib import Path
-import requests
-import os
-import argparse
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin, urlsplit
-
-
-BASE_URL = 'https://tululu.org'
-BOOKS_FOLDER = 'books'
-IMG_FOLDER = 'images'
-Path(BOOKS_FOLDER).mkdir(exist_ok=True)
-Path(IMG_FOLDER).mkdir(exist_ok=True)
-
-parser = argparse.ArgumentParser(description='Парсер книг с сайта tululu.org')
-parser.add_argument('start_id', type=int, default=1, help='Start id for downloading')
-parser.add_argument('end_id', type=int, default=2, help='Last id for downloading')
-args = parser.parse_args()
 
 
 def check_for_redirect(response):
@@ -27,65 +15,83 @@ def check_for_redirect(response):
 
 
 def parse_book_page(html_content):
-    soup = BeautifulSoup(html_content, 'lxml')
-    title_author = soup.find('h1').text.split('::')
-    cover = soup.find(class_='bookimage').find('img')['src']
+    soup = BeautifulSoup(html_content, "lxml")
+    title_author = soup.find("h1").text.split("::")
+    cover = soup.find(class_="bookimage").find("img")["src"]
     title = title_author[0].strip()
     author = title_author[1].strip()
-    comments = soup.find_all(class_='texts')
-    genres = soup.find_all('span', class_='d_book')
+    comments = soup.find_all(class_="texts")
+    genres = soup.find_all("span", class_="d_book")
     all_comments = []
     all_genres = []
     for comment in comments:
-        filtered_comment = comment.find(class_='black').text
-        all_comments.append(' '.join(filtered_comment.split()))
+        filtered_comment = comment.find(class_="black").text
+        all_comments.append(" ".join(filtered_comment.split()))
     for genre in genres:
-        all_genres.append(' '.join(genre.text.split()))
+        all_genres.append(" ".join(genre.text.split()))
     content = {
         "cover": cover,
         "title": title,
         "author": author,
         "comments": all_comments,
-        "genres": all_genres
+        "genres": all_genres,
     }
-
     return content
 
 
+def main():
+    parser = argparse.ArgumentParser(description="Парсер книг с сайта tululu.org")
+    parser.add_argument(
+        "start_id", nargs="?", type=int, default=1, help="Start id for downloading"
+    )
+    parser.add_argument(
+        "end_id", nargs="?", type=int, default=20, help="Last id for downloading"
+    )
+    args = parser.parse_args()
+
+    BASE_URL = "https://tululu.org"
+    BOOKS_FOLDER = "books"
+    IMG_FOLDER = "images"
+    Path(BOOKS_FOLDER).mkdir(exist_ok=True)
+    Path(IMG_FOLDER).mkdir(exist_ok=True)
+
+    for book in range(args.start_id, args.end_id+1):
+        download_url = f"{BASE_URL}/txt.php?id={book}"
+        parse_url = f"{BASE_URL}/b{book}"
+        try:
+            download_response = requests.get(download_url)
+            download_response.raise_for_status()
+            check_for_redirect(download_response)
+
+            parse_response = requests.get(parse_url)
+            parse_response.raise_for_status()
+            parsed_page = parse_book_page(parse_response.content)
+
+            output_filename = sanitize_filename(parsed_page["title"])
+            filepath = os.path.join(BOOKS_FOLDER, f"{output_filename}.txt")
+
+            with open(filepath, "wb") as file:
+                file.write(download_response.content)
+
+            cover_url = urljoin(BASE_URL, parsed_page["cover"])
+            cover_response = requests.get(cover_url)
+            cover_response.raise_for_status()
+
+            img_title = urlsplit(cover_url).path
+            img_filename = os.path.basename(img_title)
+            imgpath = os.path.join(IMG_FOLDER, img_filename)
+
+            with open(imgpath, "wb") as img_file:
+                img_file.write(cover_response.content)
+            print(
+                f"Book {parsed_page.get('title')} has been downloaded\n"
+                f"Genre: {parsed_page.get('genres')}\n"
+                f"Comments: {parsed_page.get('comments')}\n\n"
+            )
+        except HTTPError:
+            print(f"The book with ID {book} has not been downloaded. Passed")
+            continue
 
 
-for book in range(args.start_id, args.end_id):
-    download_url = f'{BASE_URL}/txt.php?id={book}'
-    parse_url = f'{BASE_URL}/b{book}'
-    try:
-        download_response = requests.get(download_url)
-        download_response.raise_for_status()
-        check_for_redirect(download_response)
-
-        parse_response = requests.get(parse_url)
-        parse_response.raise_for_status()
-        parsed_page = parse_book_page(parse_response.content)
-
-        output_filename = sanitize_filename(parsed_page['title'])
-        filepath = os.path.join(BOOKS_FOLDER, f'{output_filename}.txt')
-
-        with open(filepath, 'wb') as file:
-            file.write(download_response.content)
-
-        cover_url = urljoin(BASE_URL, parsed_page['cover'])
-        cover_response = requests.get(cover_url)
-        cover_response.raise_for_status()
-
-        img_title = urlsplit(cover_url).path
-        img_filename = os.path.basename(img_title)
-        imgpath = os.path.join(IMG_FOLDER, img_filename)
-
-        with open(imgpath, 'wb') as img_file:
-                    img_file.write(cover_response.content)
-        print(f"Book {parsed_page.get('title')} has been downloaded\n"
-              f"Genre: {parsed_page.get('genres')}\n"
-              f"Comments: {parsed_page.get('comments')}\n\n")
-
-    except HTTPError:
-        print(f'The book with ID {book} has not been downloaded. Passed')
-        continue
+if __name__ == "__main__":
+    main()
