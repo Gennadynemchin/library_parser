67ed99236c5431b4c9fa4eebc03544eba7256a8a
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-05-04 14:39:19 +0300

    Added parse_book_page function;

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlsplit
 
 
 def check_for_redirect(response):
@@ -13,46 +13,70 @@ def check_for_redirect(response):
             raise HTTPError
 
 
-folder = 'books'
-Path(folder).mkdir(exist_ok=True)
+def parse_book_page(html_content):
+    soup = BeautifulSoup(html_content, 'lxml')
+    title_author = soup.find('h1').text.split('::')
+    cover = soup.find(class_='bookimage').find('img')['src']
+    title = title_author[0].strip()
+    author = title_author[1].strip()
+    comments = soup.find_all(class_='texts')
+    genres = soup.find_all('span', class_='d_book')
+    all_comments = []
+    all_genres = []
+    for comment in comments:
+        filtered_comment = comment.find(class_='black').text.strip()
+        all_comments.append(filtered_comment)
+    for genre in genres:
+        filtered_genre = genre.text.strip()
+        all_genres.append(filtered_genre)
 
-for book in range(13, 14):
-    download_url = f'https://tululu.org/txt.php?id={book}'
-    title_author_url = f'https://tululu.org/b{book}'
+    content = {
+        "cover": cover,
+        "title": title,
+        "author": author,
+        "comments": all_comments,
+        "genres": all_genres
+    }
+
+    return content
+
+
+base_url = 'https://tululu.org'
+folder = 'books'
+img_folder = 'images'
+Path(folder).mkdir(exist_ok=True)
+Path(img_folder).mkdir(exist_ok=True)
+
+for book in range(1, 2):
+    download_url = f'{base_url}/txt.php?id={book}'
+    parse_url = f'{base_url}/b{book}'
     try:
         download_response = requests.get(download_url)
         download_response.raise_for_status()
         check_for_redirect(download_response)
-        title_author_response = requests.get(title_author_url)
-        title_author_response.raise_for_status()
 
-        soup = BeautifulSoup(title_author_response.text, 'lxml')
-        title_author = soup.find('h1').text.split('::')
-        cover = soup.find(class_='bookimage').find('img')['src']
-        print(cover)
-        cover_url = urljoin('https://tululu.org', cover)
-        print(cover_url)
-        title = title_author[0].strip()
-        author = title_author[1].strip()
-        output_filename = sanitize_filename(title)
+        parse_response = requests.get(parse_url)
+        parse_response.raise_for_status()
+        parsed_page = parse_book_page(parse_response.content)
+
+        cover_url = urljoin(base_url, parsed_page['cover'])
+
+        output_filename = sanitize_filename(parsed_page['title'])
         filepath = os.path.join(folder, f'{output_filename}.txt')
 
         with open(filepath, 'wb') as file:
             file.write(download_response.content)
 
+        cover_response = requests.get(cover_url)
+        cover_response.raise_for_status()
+
+        img_title = urlsplit(cover_url).path
+        img_filename = os.path.basename(img_title)
+        imgpath = os.path.join(img_folder, img_filename)
+
+        with open(imgpath, 'wb') as img_file:
+                    img_file.write(cover_response.content)
+
     except HTTPError:
         print(f'The book with ID {book} has not been downloaded. Passed')
         continue
-
-
-
-
-'''
-soup = BeautifulSoup(response.text, 'lxml')
-title_tag = soup.find('main').find('header').find('h1')
-print(title_tag.text)
-title_img = soup.find('img', class_='attachment-post-image')
-print(title_img['src'])
-page_text = soup.find(class_='entry-content').findAll('p')
-print(page_text)
-'''
