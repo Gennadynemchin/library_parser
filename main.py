b104363d0f320f7ecd2022f76a611fb831ea6819
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-05-10 18:48:36 +0300

    Added additional exceptions;

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import logging
 import requests
+from urllib3.exceptions import NewConnectionError
 from requests import HTTPError
 from pathlib import Path
 from bs4 import BeautifulSoup
@@ -58,17 +59,19 @@ def parse_book_page(html_content):
     return content
 
 
-def download_text(download_url, book_id):
+def download_text(download_url, book_id, session, retries):
     params = {'id': book_id}
-    response_download = requests.get(download_url, params=params)
+    session.mount(download_url, HTTPAdapter(max_retries=retries))
+    response_download = session.get(download_url, params=params)
     response_download.raise_for_status()
     check_for_redirect(response_download)
     return response_download.content
 
 
-def download_cover(base_url, img_url):
+def download_cover(base_url, img_url, session, retries):
     cover_url = urljoin(base_url, img_url)
-    cover_response = requests.get(cover_url)
+    session.mount(cover_url, HTTPAdapter(max_retries=retries))
+    cover_response = session.get(cover_url)
     cover_response.raise_for_status()
     check_for_redirect(cover_response)
     img_title = urlsplit(cover_url).path
@@ -84,36 +87,40 @@ def main():
 
         session = requests.Session()
         total_retries = 3
-        backoff_factor = 5
-
+        backoff_factor = 3
         retries = Retry(total=total_retries, backoff_factor=backoff_factor)
-        for book_id in trange(args.start_id, args.end_id + 1, desc='Downloading books', leave=True):
+
+        for book_id in trange(args.start_id, args.end_id + 1, desc='Task in progress', leave=True):
             download_url = f"{base_url}/txt.php"
             parse_url = f"{base_url}/b{book_id}"
             try:
-                response_download = download_text(download_url, book_id)
-                # response_parse = requests.get(parse_url)
+                response_download = download_text(download_url, book_id, session, retries)
+
                 session.mount(parse_url, HTTPAdapter(max_retries=retries))
                 response_parse = session.get(parse_url)
-                # print(response_parse.history)
                 response_parse.raise_for_status()
+
                 parsed_page = parse_book_page(response_parse.content)
+                cover_response = download_cover(base_url, parsed_page["cover"], session, retries)
 
                 output_filename = sanitize_filename(parsed_page["title"])
                 filepath = os.path.join(books_folder, f"{output_filename}.txt")
+                imgpath = os.path.join(img_folder, cover_response['filename'])
+
                 with open(filepath, "wb") as file:
                     file.write(response_download)
-                cover_response = download_cover(base_url, parsed_page["cover"])
-                imgpath = os.path.join(img_folder, cover_response['filename'])
                 with open(imgpath, "wb") as img_file:
                     img_file.write(cover_response['img'])
                 log.info(f"Book {parsed_page.get('title')} with ID {book_id} has been downloaded")
-            except (requests.exceptions.HTTPError, requests.RequestException, requests.exceptions.Timeout):
-                log.info("Try to reconnect after 5 seconds")
-                sleep(5)
             except HTTPError:
                 log.info(f"The book with ID {book_id} has not been downloaded. Passed")
                 continue
+            except (requests.exceptions.HTTPError,
+                    requests.RequestException,
+                    NewConnectionError,
+                    requests.exceptions.Timeout):
+                # sleep(3)
+                log.info(f"Try to reconnect soon. The book with ID {book_id} passed")
 
 
 if __name__ == "__main__":
